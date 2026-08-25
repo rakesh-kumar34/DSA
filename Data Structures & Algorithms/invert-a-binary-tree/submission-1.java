751d2abd9719a03b221a8eaaf0cdeda615622dfb
Rakesh Kumar <rakeshcgpt@gmail.com>
2026-08-25 16:45:40 -0400

    Add: invert-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-1.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+        if (root.left == null && root.right == null) return root;
+        
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            TreeNode curr = q.poll();
+
+            // swap left and right nodes
+            TreeNode temp = curr.left;
+            curr.left = curr.right;
+            curr.right = temp;
+
+            if (curr.left != null) q.offer(curr.left);
+            if (curr.right != null) q.offer(curr.right);
+        }
+        return root;
+    }
+}
